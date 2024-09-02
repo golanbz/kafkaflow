08366615152f2bb091b9e67115abb61450046c84
golanbz <golanbenzaken@gmail.com>
2024-09-02 14:50:12 +0300

    Introduced a new sample project designed specifically to evaluate the support for the cooperative sticky strategy, allowing for more thorough testing and analysis.

---
+++ samples/KafkaFlow.Sample.CooperativeSticky/Program.cs
@@ -0,0 +1,53 @@
+﻿using Confluent.Kafka;
+using KafkaFlow;
+using KafkaFlow.Sample.CooperativeSticky;
+using KafkaFlow.Serializer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AutoOffsetReset = KafkaFlow.AutoOffsetReset;
+
+const string producerName = "PrintConsole";
+const string topicName = "sample-topic";
+var hostBuilder = new HostBuilder();
+hostBuilder.ConfigureServices(services =>
+    services.AddHostedService<HostedService>().AddKafka(
+        kafka => kafka
+            .UseConsoleLog()
+            .AddCluster(
+                cluster => cluster
+                    .WithBrokers(new[] { "localhost:9092" })
+                    .CreateTopicIfNotExists(topicName, 6, 1)
+                    .AddProducer(
+                        producerName,
+                        producer => producer
+                            .DefaultTopic(topicName)
+                            .AddMiddlewares(m => m.AddSerializer<ProtobufNetSerializer>())
+                    )
+                    .AddConsumer(
+                        consumer => consumer
+                            .WithConsumerConfig(new ConsumerConfig
+                            {
+                                PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky,
+                                AutoCommitIntervalMs = 100
+                            })
+                            .Topic(topicName)
+                            .WithGroupId("print-console-handler")
+                            .WithBufferSize(100)
+                            .WithWorkersCount(3)
+                            .WithAutoCommitIntervalMs(10)
+                            .WithAutoOffsetReset(AutoOffsetReset.Latest)
+                            .AddMiddlewares(
+                                middlewares => middlewares
+                                    .AddDeserializer<ProtobufNetDeserializer>()
+                                    .AddTypedHandlers(h => h.AddHandler<PrintConsoleHandler>())
+                            )
+                    )
+            )
+    ));
+
+var build = hostBuilder.Build();
+var kafkaBus = build.Services.CreateKafkaBus();
+await kafkaBus.StartAsync();
+
+await build.RunAsync();
+await kafkaBus.StopAsync();
